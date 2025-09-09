aeed29d41800252787ee20753f3bb582110b829f
Anna10334 <912134@senacminas.edu.br>
2025-09-09 16:13:08 -0300

    Etapa 2: Adiciona a função de subtração

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,15 @@
 # Atividade 13 - Versionamento em Etapas com Git e VS Code
+# Etapa 1: O Início do Projeto (1º Commit)
 
 def soma_de_dois_numeros (numero1, numero2):
     soma_de_dois_numeros = numero1 + numero2
     return soma_de_dois_numeros
+
+#Etapa 2: Adicionando Funcionalidade (2º Commit)
+
+def subtracao_de_dois_numeros (numero1, numero2):
+    subtracao_de_dois_numeros = numero1 - numero2
+    return subtracao_de_dois_numeros
+
+
+
