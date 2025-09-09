4b69fbe70fc9156055085cc17f8162bd31255762
Anna10334 <912134@senacminas.edu.br>
2025-09-09 16:08:24 -0300

    Etapa 1: Adiciona a função de soma

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+# Atividade 13 - Versionamento em Etapas com Git e VS Code
+
+def soma_de_dois_numeros (numero1, numero2):
+    soma_de_dois_numeros = numero1 + numero2
+    return soma_de_dois_numeros
